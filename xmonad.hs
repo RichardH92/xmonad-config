cc1f64ee255b1b89f5e3e29dba1123634c6111cb
Richard Hart <richardhart92@gmail.com>
2015-07-16 11:02:49 -0400

    First Commit

---
+++ xmonad.hs
@@ -0,0 +1,249 @@
+import XMonad
+import qualified XMonad.StackSet as W
+import qualified Data.Map        as M
+import Data.Monoid
+ 
+import XMonad.Actions.CycleWS
+import XMonad.Actions.FlexibleResize as Flex
+import XMonad.Actions.Search
+import XMonad.Actions.UpdatePointer
+import XMonad.Actions.WindowGo
+import qualified XMonad.Actions.Submap as SM
+ 
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+ 
+import XMonad.Layout.LayoutHints
+import XMonad.Layout.NoBorders
+import XMonad.Layout.ResizableTile
+import XMonad.Layout.Tabbed
+import XMonad.Layout.ToggleLayouts
+import XMonad.Layout.WindowArranger
+import XMonad.Layout.Mosaic
+ 
+import XMonad.Prompt
+import XMonad.Prompt.Input
+import XMonad.Prompt.RunOrRaise
+import XMonad.Prompt.Shell
+import XMonad.Prompt.Window
+ 
+import XMonad.Util.Run
+import XMonad.Util.Scratchpad
+import XMonad.Util.WorkspaceCompare
+import XMonad.Util.XSelection
+ 
+-- for dbus (gnome-session hang)
+import System.Environment
+import System.Cmd
+import Control.Concurrent
+
+
+
+myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
+    -- terminals
+    [ ((modMask,                 xK_Return), spawn $ XMonad.terminal conf)
+    , ((modMask .|. shiftMask,   xK_Return), spawn "urxvt -pe tabbed")
+ 
+    -- file manager
+    , ((modMask,                 xK_Up    ), spawn "nautilus ~")
+ 
+    -- shell/window prompts
+    , ((modMask,                 xK_space ), runOrRaisePrompt mySP)
+    , ((modMask .|. shiftMask,   xK_space ), shellPrompt mySP)
+    , ((modMask .|. controlMask, xK_space), windowPromptGoto mySP)
+ 
+    -- browser
+    --, ((modMask,               xK_b     ), runOrRaise
+    --    "firefox" (className =? "Firefox"))
+ 
+    -- cycle through workspaces
+    , ((modMask,               xK_Right ), moveTo Next (WSIs (return $ not . (=="SP") . W.tag)))
+    , ((modMask,               xK_Left  ), moveTo Prev (WSIs (return $ not . (=="SP") . W.tag)))
+ 
+    -- move windows through workspaces
+    , ((modMask .|. shiftMask, xK_Right ), shiftTo Next (WSIs (return $ not . (=="SP") . W.tag)))
+    , ((modMask .|. shiftMask, xK_Left  ), shiftTo Prev (WSIs (return $ not . (=="SP") . W.tag)))
+    , ((modMask .|. controlMask, xK_Right), shiftTo Next EmptyWS)
+    , ((modMask .|. controlMask, xK_Left), shiftTo Prev EmptyWS)
+ 
+    -- Move focus to the next/previous window
+    , ((modMask,               xK_j     ), windows W.focusDown)
+    , ((modMask,               xK_Tab   ), windows W.focusDown)
+    , ((mod1Mask,              xK_Tab   ), windows W.focusDown)
+    , ((modMask,               xK_k     ), windows W.focusUp)
+    , ((modMask .|. shiftMask, xK_Tab   ), windows W.focusUp)
+    , ((mod1Mask .|. shiftMask, xK_Tab  ), windows W.focusUp)
+ 
+    -- Swap the focused window with next/prev window
+    , ((modMask .|. shiftMask, xK_j     ), windows W.swapDown)
+    , ((modMask .|. shiftMask, xK_k     ), windows W.swapUp)
+ 
+    -- Shrink/Expand the master area
+    , ((modMask,               xK_h     ), sendMessage Shrink)
+    , ((modMask,               xK_l     ), sendMessage Expand)
+    , ((modMask .|. shiftMask, xK_h     ), sendMessage MirrorShrink)
+    , ((modMask .|. shiftMask, xK_l     ), sendMessage MirrorExpand)
+ 
+    -- Swap the focused window and the master window
+    , ((modMask,            xK_semicolon), windows W.swapMaster)
+ 
+    -- Increment/Deincrement the number of windows in the master area
+    , ((modMask,               xK_comma ), sendMessage (IncMasterN 1))
+    , ((modMask,               xK_period), sendMessage (IncMasterN (-1)))
+ 
+    -- Resize viewed windows to the correct size
+    , ((modMask,               xK_n     ), refresh)
+    -- Reset layout of current workspace
+    , ((modMask .|. shiftMask, xK_n     ), setLayout $ XMonad.layoutHook conf)
+
+    -- toggle focused window fullscreen
+    , ((modMask,               xK_m     ), sendMessage (Toggle "Full")
+    >> (dynamicLogString myPP >>= \d->safeSpawn "gnome-osd-client" [d]))
+ 
+    -- Push window back into tiling
+    , ((modMask,               xK_s     ), withFocused $ windows . W.sink)
+ 
+    -- toggle the status bar gap
+    , ((modMask,               xK_f     ), sendMessage ToggleStruts)
+ 
+    -- close focused window
+    , ((modMask              , xK_w     ), kill)
+ 
+    -- Restart xmonad
+    , ((modMask              , xK_q     ),
+        broadcastMessage ReleaseResources >> restart "xmonad" True)
+    ]
+ 
+    ++
+    -- mod-[1..9], Switch to workspace N
+    -- mod-shift-[1..9], Move client to workspace N
+    [ ((m .|. modMask, k), windows $ f i)
+        | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
+        , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)] ]
+	
+
+	
+myMouseBindings (XConfig {XMonad.modMask = modMask}) = M.fromList $
+    -- mod-button1, Set the window to floating mode and move by dragging
+    [ ((modMask, button1), (\w -> focus w >> mouseMoveWindow w))
+    -- mod-button2, Raise the window to the top of the stack
+    , ((modMask, button2), (\w -> focus w >> windows W.swapMaster))
+    -- mod-button3, Set the window to floating mode and resize by dragging
+    , ((modMask, button3), (\w -> focus w >> Flex.mouseResizeWindow w)) ]
+	
+	
+	
+-- decoration theme
+myDeco = defaultTheme
+    { activeColor         = "orange"
+    , inactiveColor       = "#222222"
+    , urgentColor         = "yellow"
+    , activeBorderColor   = "orange"
+    , inactiveBorderColor = "#222222"
+    , urgentBorderColor   = "yellow"
+    , activeTextColor     = "orange"
+    , inactiveTextColor   = "#222222"
+    , urgentTextColor     = "yellow"
+    , decoHeight          = 10 }
+ 
+ 
+ 
+-- tab theme
+myTab = defaultTheme
+    { activeColor         = "black"
+    , inactiveColor       = "black"
+    , urgentColor         = "yellow"
+    , activeBorderColor   = "orange"
+    , inactiveBorderColor = "#222222"
+    , urgentBorderColor   = "black"
+    , activeTextColor     = "orange"
+    , inactiveTextColor   = "#222222"
+    , urgentTextColor     = "yellow" }
+ 
+ 
+ 
+-- shell prompt theme
+mySP = defaultXPConfig
+    { bgColor           = "black"
+    , fgColor           = "white"
+    , bgHLight          = "gray"
+    , fgHLight          = "black"
+    , borderColor       = "orange"
+    , promptBorderWidth = 1
+    , position          = Bottom
+    , height            = 20
+    --, autoComplete      = Just 1000
+    , historySize       = 1000 }
+ 
+ 
+ 
+-- dynamicLog theme (suppress everything but layout)
+myPP = defaultPP
+    { ppLayout  = (\ x -> case x of
+      "Hinted ResizableTall"        -> "[|]"
+      "Mirror Hinted ResizableTall" -> "[-]"
+      "Hinted Tabbed Simplest"      -> "[T]"
+      "Full"                 -> "[ ]"
+      _                      -> x )
+    , ppCurrent         = const ""
+    , ppVisible         = const ""
+    , ppHidden          = const ""
+    , ppHiddenNoWindows = const ""
+    , ppUrgent          = const ""
+    , ppTitle           = const ""
+    , ppWsSep           = ""
+    , ppSep             = "" }
+
+	
+	
+-- layouts
+myLayout = avoidStruts $ toggleLayouts (noBorders Full)
+    (smartBorders (tiled ||| mosaic 2 [3,2] ||| Mirror tiled ||| layoutHints (tabbed shrinkText myTab)))
+    where
+        tiled   = layoutHints $ ResizableTall nmaster delta ratio []
+        nmaster = 1
+        delta   = 2/100
+        ratio   = 1/2
+ 
+ 
+ 
+-- special windows
+myManageHook = composeAll
+    [ className =? "vlc"        --> doFloat
+    , className =? "Gimp"           --> doFloat
+    , resource  =? "desktop_window" --> doIgnore
+    , isFullscreen                  --> doFullFloat
+    , scratchpadManageHook $ W.RationalRect 0 0 1 0.42
+    , manageDocks ] <+> manageHook defaultConfig
+ 
+ 
+ 
+		
+main = do
+	xmproc <- spawnPipe "xmobar"
+
+    env <- getEnvironment
+    case lookup "DESKTOP_AUTOSTART_ID" env of
+        Just id -> do
+            forkIO $ (>> return ()) $ rawSystem "dbus-send" ["--session","--print-reply=string","--dest=org.gnome.SessionManager","/org/gnome/SessionManager","org.gnome.SessionManager.RegisterClient","string:xmonad","string:"++id]
+            return ()
+        Nothing -> return ()
+    xmonad $ defaultConfig 
+		{ terminal           = "urxvt"
+		, borderWidth        = 2
+		, normalBorderColor  = "black"
+		, focusedBorderColor = "orange"
+		, focusFollowsMouse  = True
+		, modMask            = mod4Mask
+		, keys               = myKeys
+		, mouseBindings      = myMouseBindings
+		, layoutHook         = myLayout
+		, handleEventHook    = ewmhDesktopsEventHook
+		, startupHook        = ewmhDesktopsStartup
+		, logHook            = myLogHook
+		, manageHook         = myManageHook
+		}
+	
+	
